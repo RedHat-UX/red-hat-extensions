77950f9024ea3c31ff39c4a6ca4a544acf54d611
Steven Spriggs <steven.spriggs@gmail.com>
2024-07-29 16:25:35 -0400

    fix(grid): add row-span and fix style overlap

--- elements/rhx-grid/rhx-grid.ts
+++ elements/rhx-grid/rhx-grid.ts
@@ -2,27 +2,18 @@ import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 
-import styles from './rhx-grid.css';
-
-/**
- * TODO: We don't need shadowdom here, but for the sake of ensuring
- * nothing blows up in dev leaving this file in place for now.
- */
-
 /**
  * Grid
  * @slot - Place element content here
  */
 @customElement('rhx-grid')
 export class RhxGrid extends LitElement {
-  static readonly styles = [styles];
+  @property({ reflect: true }) columns?: string;
 
   @property({ reflect: true }) query?: 'media' | 'container' | null = null;
 
-  render() {
-    return html`
-      <slot></slot>
-    `;
+  createRenderRoot() {
+    return this;
   }
 }
 
