93301cd589b5364bb20fce1f8586a9cbc9b78503
Steven Spriggs <steven.spriggs@gmail.com>
2025-01-07 12:54:36 -0500

    fix(grid): missing extension

--- elements/rhx-grid/rhx-grid.ts
+++ elements/rhx-grid/rhx-grid.ts
@@ -2,7 +2,7 @@ import { LitElement } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
 
-import './rhx-grid-item';
+import './rhx-grid-item.js';
 
 /**
  * Grid
