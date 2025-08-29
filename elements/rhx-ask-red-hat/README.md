476b3e57a99eb98d66c854b22ffaa7539bfc153a
Steven Spriggs <steven.spriggs@gmail.com>
2025-08-29 09:33:04 -0400

    fix(ask-red-hat): remove auth checks, improve scheme handling, add rh-icon

--- elements/rhx-ask-red-hat/README.md
+++ elements/rhx-ask-red-hat/README.md
@@ -1,26 +1,9 @@
 # Ask Red Hat
 
-Ask Red Hat is a link that appears if the authenticated user has access to the Ask Red Hat AI assistant.
+A stylized link that links to the Ask Red Hat AI assistant.
 
 ## Usage
 
 ```html
 <rhx-ask-red-hat></rhx-ask-red-hat>
-
-<script type="module">
-  import '@rhdx/elements/rhx-ask-red-hat/rhx-ask-red-hat.js';
-
-  const cpAsk = document.querySelector('rhx-ask-red-hat');
-  await cpAsk.updateComplete;
-  cpAsk.healthCheckEndpoint = 'http://access.redhat.com/api/ask/v1/health';
-  cpAsk.hydraEndpoint = 'https://access.redhat.com/hydra/rest/contacts/sso/current';
-
-  // Pseudo user auth
-  if (user.session.isAuthenticated()) {
-    // get values session pass them to cpAsk
-    cpAsk.authToken = user.session.getToken();
-    cpAsk.userId = user.session.getUserId();
-  }
-</script>
-
 ```
--- elements/rhx-ask-red-hat/rhx-ask-red-hat.ts
+++ elements/rhx-ask-red-hat/rhx-ask-red-hat.ts
@@ -1,123 +1,40 @@
+import type { IconNameFor, IconSetName } from '@rhds/icons';
+
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators/custom-element.js';
 import { property } from 'lit/decorators/property.js';
-import { state } from 'lit/decorators/state.js';
+
+import '@rhds/elements/rh-icon/rh-icon.js';
 
 import styles from './rhx-ask-red-hat.css';
 
-interface HydraResponse {
-  isEntitled: boolean;
-  isInternal: boolean;
-}
-
 /**
- * Ask Red Hat is a link that appears if the authenticated user has access to the Ask Red Hat AI assistant.
+ * A stylized link to the Ask Red Hat AI assistant.
+ * @summary A stylized link to the Ask Red Hat AI assistant.
  */
 @customElement('rhx-ask-red-hat')
 export class RhxAskRedHat extends LitElement {
   static readonly styles = [styles];
 
-  @state()
-  private _hasAccess = false;
+  /** When set overrides the default link of https://access.redhat.com/ask to the Ask Red Hat AI assistant. */
+  @property() href = 'https://access.redhat.com/ask';
 
-  @property({ attribute: false }) href = 'https://access.redhat.com/ask';
+  /** When set overrides the default icon set of 'ui' to the icon set of the icon. */
+  @property({ attribute: 'icon-set' }) set: IconSetName = 'ui';
 
-  @property({ attribute: false }) healthCheckEndpoint?: string;
+  /** When set overrides the default icon of 'new-fill' to the icon. */
+  @property() icon: IconNameFor<IconSetName> = 'new-fill';
 
-  @property({ attribute: false }) hydraEndpoint?: string;
-
-  @property({ attribute: false }) authToken?: string;
-
-  @property({ attribute: false }) userId?: string;
-
+  /** When set overrides the default link text of 'Ask Red Hat' to the link text. */
   @property({ attribute: 'link-text' }) linkText = 'Ask Red Hat';
-
-  async updated(): Promise<void> {
-    if (await this.#healthCheck()) {
-      this._hasAccess = await this.#checkAccess();
-    }
-  }
 
   render() {
     return html`
-      ${this._hasAccess ? html`
-        <a href="${this.href}">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="m30.155 17.267-9.422-2-2-9.422a.75.75 0 0 0-1.466 0l-2 9.422-9.422 2a.75.75 0 0 0 0 1.466l9.422 2 2 9.422a.75.75 0 0 0 1.466 0l2-9.422 9.422-2a.75.75 0 0 0 0-1.466ZM6 11.75a.75.75 0 0 0 .733-.595l.775-3.647 3.647-.775a.75.75 0 0 0 0-1.466l-3.647-.775L6.733.845a.75.75 0 0 0-1.466 0l-.775 3.647-3.647.775a.75.75 0 0 0 0 1.466l3.647.775.775 3.647A.75.75 0 0 0 6 11.75Z"></path></svg>
-          ${this.linkText}
-        </a>
-      ` : ``}
+      <a href="${this.href}">
+        <rh-icon icon="${this.icon}" set="${this.set}"></rh-icon>
+        ${this.linkText}
+      </a>
     `;
-  }
-
-  async #healthCheck(): Promise<boolean> {
-    if (!this.healthCheckEndpoint) {
-      return false;
-    }
-    const url = this.healthCheckEndpoint;
-
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => {
-      controller.abort();
-      return false;
-    }, 10000);
-
-    try {
-      const healthCheckApiResponse = await fetch(url, {
-        signal: controller.signal,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (!healthCheckApiResponse.ok) {
-        return false;
-      }
-
-      const json = await healthCheckApiResponse.json();
-      const { status } = json.status;
-      if (status === 'OK') {
-        clearTimeout(timeoutId);
-      }
-      return true;
-    } catch {
-      return false;
-    }
-  }
-
-  async #checkAccess(): Promise<boolean> {
-    if (!this.hydraEndpoint || !this.authToken || !this.userId) {
-      sessionStorage.removeItem('ifd-access');
-      return false;
-    }
-    const sessionStore = sessionStorage.getItem('ifd-access');
-    if (sessionStore !== null) {
-      return (sessionStore === 'true');
-    } else {
-      const controller = new AbortController();
-      const timeoutId = setTimeout(() => {
-        controller.abort();
-        return false;
-      }, 10000);
-      const options = {
-        method: 'GET',
-        signal: controller.signal,
-        headers: {
-          'Authorization': `Bearer ${this.authToken}`,
-          'X-User-ID': this.userId,
-        },
-      };
-      const response = await fetch(`${this.hydraEndpoint}`, options);
-      const data = await response.json() as HydraResponse;
-      if (data && (data.isEntitled || data.isInternal)) {
-        sessionStorage.setItem('ifd-access', 'true');
-        clearTimeout(timeoutId);
-        return true;
-      } else {
-        sessionStorage.setItem('ifd-access', 'false');
-        clearTimeout(timeoutId);
-        return false;
-      }
-    }
   }
 }
 
